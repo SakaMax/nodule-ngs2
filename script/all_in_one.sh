dd821ec05b0db5312999b50a1386ae05f667a198
Kohsuke Sakaguchi <sanbonkihara@gmail.com>
2020-12-01 17:35:37 +0900

    change relative path to absolute path.

--- script/all_in_one.sh
+++ script/all_in_one.sh
@@ -5,31 +5,35 @@
 # Note:
 # You can look at the latest result of fastp in http://<address-to-machine>:80/latest.html
 
+# Check where this script is.
+# see https://codechacha.com/ja/how-to-get-path-of-bash-script/
+relative_dir=`dirname "$0"`
+canocnical_dir=`readlink -f $relative_dir`
 
 # create folder
 # ./folder_temp/folder_time
 folder_time=`date +%Y_%m%d_%H%M`
 folder_data="data"
-folder_name="${folder_data}/${folder_time}"
+out_folder_name="${canocnical_dir}/${folder_data}/${folder_time}"
 
 # step 1
 # rename reads by tag and cut tag seqences
 echo "step 1"
-mkdir -p "${folder_name}/tag_removed" && python3 -m cutadapt --no-indels --discard-untrimmed \
-    -g file:./source/R1tag.fasta -G file:./source/R2tag.fasta -y ' {name}' \
-    -o "${folder_name}/tag_removed/R1.fastq" -p "${folder_name}/tag_removed/R2.fastq" $1 $2
+mkdir -p "${out_folder_name}/tag_removed" && python3 -m cutadapt --no-indels --discard-untrimmed \
+    -g "file:${canocnical_dir}/source/R1tag.fasta" -G file:"${canocnical_dir}/source/R2tag.fasta" -y ' {name}' \
+    -o "${out_folder_name}/tag_removed/R1.fastq" -p "${out_folder_name}/tag_removed/R2.fastq" $1 $2
     
 # step 2
 # remove primer
 echo "step 2"
-mkdir -p "${folder_name}/primer_removed" && python3 -m cutadapt --discard-untrimmed \
-    -g file:./source/R1primer.fasta -G file:./source/R2primer.fasta \
-    -o "${folder_name}/primer_removed/R1.fastq" -p "${folder_name}/primer_removed/R2.fastq" \
-    "${folder_name}/tag_removed/R1.fastq" "${folder_name}/tag_removed/R2.fastq"
+mkdir -p "${out_folder_name}/primer_removed" && python3 -m cutadapt --discard-untrimmed \
+    -g "file::${canocnical_dir}/source/R1primer.fasta" -G "file::${canocnical_dir}/source/R2primer.fasta" \
+    -o "${out_folder_name}/primer_removed/R1.fastq" -p "${out_folder_name}/primer_removed/R2.fastq" \
+    "${out_folder_name}/tag_removed/R1.fastq" "${out_folder_name}/tag_removed/R2.fastq"
 
 # step 3
 # filtering by fastp
 echo "step 3"
-mkdir -p "${folder_name}/fastp" && fastp -i "${folder_name}/primer_removed/R1.fastq" -I "${folder_name}/primer_removed/R2.fastq" \
-    -3 -o "${folder_name}/fastp/R1.fastq" -O "${folder_name}/fastp/R2.fastq" \
-    -h "${folder_name}/fastp/report.html" -q 30 -n 5 -A && cp "${folder_name}/fastp/report.html" /var/www/html/latest.html
+mkdir -p "${out_folder_name}/fastp" && fastp -i "${out_folder_name}/primer_removed/R1.fastq" -I "${out_folder_name}/primer_removed/R2.fastq" \
+    -3 -o "${out_folder_name}/fastp/R1.fastq" -O "${out_folder_name}/fastp/R2.fastq" \
+    -h "${out_folder_name}/fastp/report.html" -q 30 -n 5 -A && cp "${out_folder_name}/fastp/report.html" /var/www/html/latest.html
